ce56889f18d002a72836e9dc6a437b786bdebd33
Jinsong Bai <bjstry@163.com>
2016-09-11 03:03:32 +0800

    update

--- codes/C/IndexC.class.php
+++ codes/C/IndexC.class.php
@@ -6,7 +6,7 @@ class IndexC extends C{
 		$row['keywords'] = "硕星，硕星信息，西安硕星信息技术有限公司";
 		$row['title'] = "专业化的服务";
 		$row['t_info'] = '专业化的服务是保障客户成功的基础！';
-		$this->assign('title','硕星，西安硕星，服务器，硕星服务器，西安硕星信息技术有限公司，西安硕星信息技术有限公司欢迎您！');
+		$this->assign('title','硕星，硕星信息，西安硕星信息有限公司，西安硕星信息技术有限公司欢迎您，星蕴，星蕴服务器，星蕴设备');
 		$this->assign('instatus',"uk-active");
 		$this->assign('row',$row);
 		$this->assign('mycss',"<link rel='stylesheet' href='"._P_."/css/components/slideshow.min.css'><link rel='stylesheet' href='"._P_."/css/index.css'>");
